5928d1d4e3f71d5e8d9018a7ec0246dcf5f8febf
ajlino <38694859+ajlino@users.noreply.github.com>
2020-09-28 22:17:29 -0400

    created objects for cards and buttons
    add press return for new game

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import random
 
 from collections import namedtuple
 
-#global variables
-global deck, hand
 
 # Initializing PyGame
 pygame.init()
@@ -24,7 +22,6 @@ LIGHT_RED = (120, 0, 0)
 small_font = pygame.font.Font(None, 32)
 large_font = pygame.font.Font(None, 50)
 
-
 # Setting up the screen, background
 # WINDOW SIZE
 WIDTH = 1600
@@ -36,6 +33,8 @@ MARGIN_TOP = 150
 scaleFactor = 1.4
 card_width = int(360 / scaleFactor)
 card_height = int(540 / scaleFactor)
+buttonY = 800
+
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill(GRAY)
@@ -47,34 +46,49 @@ icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
 
-
 # Card class definition
 class Card(namedtuple('Card', 'face, suit')):
     def __repr__(self):
         return ''.join(self)
 
+# Button class definition
+class Button:
+    def __init__(self, position, color, value, locationX, locationY):
+        self.position = position
+        self.color = color
+        self.value = value
+        self.locationX = locationX
+        self.locationY = locationY
+
+    def render_button(self):
+        if self.value:
+            self.color = RED
+        else:
+            self.color = WHITE
+        button = large_font.render("HOLD", True, self.color)
+        button_rect = button.get_rect()
+        button_rect.center = (self.locationX, self.locationY)
+        screen.blit(button, button_rect)
+
+
+
+#Ingredients for creating and ranking cards
 suit = 'h d c s'.split()
-# ordered strings of faces
 faces = '2 3 4 5 6 7 8 9 10 j q k a'
 lowaces = 'a 2 3 4 5 6 7 8 9 10 j q k'
-# faces as lists
 face = faces.split()
 lowace = lowaces.split()
 
+def shuffle_and_deal():
 
-def create_deck():
+    #create the deck. Creates 52 Card Objects.
     deck = []
-    # Loop for every type of suit
     for suitX in suit:
-
-        # Loop for every type of card in a suit
         for card in face:
             # Adding the card to the deck
-            deck.append(Card(card, suitX))
+             deck.append(Card(card, suitX))
 
-    return deck
-
-def deal_hand(deck):
+    #deal the hand
     hand=[]
     for x in range(1,6):
         # Choose the card from the deck
@@ -82,7 +96,14 @@ def deal_hand(deck):
         hand.append((current_card))
         # Remove the card from the deck
         deck.remove(current_card)
-    return (hand)
+
+    #render the cards on screen
+        renderHand(hand)
+    #render the buttons
+        choices = [False, False, False, False, False]
+        set_buttons(choices)
+
+    return  deck, hand, choices
 
 def renderHand(hand):
     offset = 0
@@ -94,24 +115,30 @@ def renderHand(hand):
         offset += 320
         MARGIN_LEFT= 0
 
+def set_buttons(choices):
+    pos1 = MARGIN_LEFT + (card_width/2)
+    spacing = card_width + 20
+    buttons = []
+    buttons.append(Button(0,WHITE,choices[0], pos1, buttonY))
+    buttons.append(Button(1,WHITE,choices[1],spacing, buttonY))
+    buttons.append(Button(2,WHITE,choices[2],spacing*2, buttonY))
+    buttons.append(Button(3,WHITE,choices[3],spacing*3, buttonY))
+    buttons.append(Button(4,WHITE,choices[4],spacing*4, buttonY))
+
+    for button in buttons:
+        button.render_button()
+
+def draw(choices, hand, deck):
+    n=0
+    for choice in choices:
+        if choice == False:
+            current_card = random.choice(deck)
+            hand[n] = current_card
+            deck.remove(current_card)
+        n+=1
+    return choices, hand, deck
 
 
-
-def draw(choice):
-    if choice[1] == "hold":
-        current_card = random.choice(deck)
-        hand.append((current_card))
-        # Remove the card from the deck
-        deck.remove(current_card)
-
-
-def set_buttons():
-    #Setting up buttons on screen
-    screen.blit(button1, button1_rect)
-    screen.blit(button2, button2_rect)
-    screen.blit(button3, button3_rect)
-    screen.blit(button4, button4_rect)
-    screen.blit(button5, button5_rect)
 
 
 
@@ -125,7 +152,6 @@ def straightflush(hand):
         return 'straight-flush', ordered[-1].face
     return False
 
-
 def fourofakind(hand):
     allfaces = [f for f, s in hand]
     allftypes = set(allfaces)
@@ -137,7 +163,6 @@ def fourofakind(hand):
             return 'four-of-a-kind', [f, allftypes.pop()]
     else:
         return False
-
 
 def fullhouse(hand):
     allfaces = [f for f, s in hand]
@@ -151,7 +176,6 @@ def fullhouse(hand):
     else:
         return False
 
-
 def flush(hand):
     allstypes = {s for f, s in hand}
     if len(allstypes) == 1:
@@ -161,7 +185,6 @@ def flush(hand):
                                reverse=True)
     return False
 
-
 def straight(hand):
     f, fs = ((lowace, lowaces) if any(card.face == '2' for card in hand)
              else (face, faces))
@@ -170,7 +193,6 @@ def straight(hand):
     if ' '.join(card.face for card in ordered) in fs:
         return 'straight', ordered[-1].face
     return False
-
 
 def threeofakind(hand):
     allfaces = [f for f, s in hand]
@@ -186,7 +208,6 @@ def threeofakind(hand):
                            reverse=True))
     else:
         return False
-
 
 def twopair(hand):
     allfaces = [f for f, s in hand]
@@ -220,18 +241,15 @@ def onepair(hand):
                                       key=lambda f: face.index(f),
                                       reverse=True)
 
-
 def highcard(hand):
     allfaces = [f for f, s in hand]
     return 'high-card', sorted(allfaces,
                                key=lambda f: face.index(f),
                                reverse=True)
 
-
 handrankorder = (straightflush, fourofakind, fullhouse,
                  flush, straight, threeofakind,
                  twopair, onepairJacks, onepair, highcard)
-
 
 def rank(hand):
     # hand = handy(cards)
@@ -242,124 +260,71 @@ def rank(hand):
     assert rank, "Invalid: Failed to rank cards: %r" % cards
     return rank
 
-
-#Deal the hand
-
-deck = create_deck()
-
-hand = deal_hand(deck)
-
-#debug
-print (hand)
-
-renderHand(hand)
-
-#clear the choices
-choice = [False, False, False, False, False]
-
-
-# Create Buttons
-buttonMargin = 20
-buttonSpacing = (buttonMargin + (card_width))
-button1 = large_font.render("Hold", True, WHITE)
-button1_rect = button1.get_rect()
-button1_rect.center = (buttonSpacing/2, 600)
-
-button2 = large_font.render("Hold", True, WHITE)
-button2_rect = button2.get_rect()
-button2_rect.center = (buttonSpacing*1.5, 600)
-
-button3 = large_font.render("Hold", True, WHITE)
-button3_rect = button3.get_rect()
-button3_rect.center = (buttonSpacing*2.5, 600)
-
-button4 = large_font.render("Hold", True, WHITE)
-button4_rect = button4.get_rect()
-button4_rect.center = (buttonSpacing*3.5, 600)
-
-button5 = large_font.render("Hold", True, WHITE)
-button5_rect = button5.get_rect()
-button5_rect.center = (buttonSpacing*4.5, 600)
-
-
-
+newGame = True
 
 # The GAME LOOP
 while True:
 
-    # Loop events occuring inside the game window
-    for event in pygame.event.get():
+    return_as_tuple = shuffle_and_deal()
+    deck = return_as_tuple[0]
+    hand = return_as_tuple[1]
+    choices = return_as_tuple[2]
 
+    print (hand)
+    newGame = False
 
-        # set up buttons
-        set_buttons()
+    while newGame == False:
 
-        # Qutting event
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
+        # Loop events occuring inside the game window
+        for event in pygame.event.get():
+            # Qutting event
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-        #determine if key was clicked (1-5)
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                choice[0] = not choice[0]
-                if choice[0]:
-                    button1 = large_font.render("Hold", True, RED)
-                else:
-                    button1 = large_font.render("Hold", True, WHITE)
-            elif event.key == pygame.K_2:
-                choice[1] = not choice[1]
-                if choice[1]:
-                    button2 = large_font.render("Hold", True, RED)
-                else:
-                    button2 = large_font.render("Hold", True, WHITE)
-            elif event.key == pygame.K_3:
-                choice[2] = not choice[2]
-                if choice[2]:
-                    button3 = large_font.render("Hold", True, RED)
-                else:
-                    button3 = large_font.render("Hold", True, WHITE)
-            elif event.key == pygame.K_4:
-                choice[3] = not choice[3]
-                if choice[3]:
-                    button4 = large_font.render("Hold", True, RED)
-                else:
-                    button4 = large_font.render("Hold", True, WHITE)
-            elif event.key == pygame.K_5:
-                choice[4] = not choice[4]
-                if choice[4]:
-                    button5 = large_font.render("Hold", True, RED)
-                else:
-                    button5 = large_font.render("Hold", True, WHITE)
-            elif event.key == pygame.K_SPACE:
-                n = 0
-                for x in choice:
-                    if x == False:
-                        newCard =random.choice(deck)
-                        hand[n] = newCard
-                        deck.remove(newCard)
-                        print(n)
-                    n+=1
+            # determine if key was clicked (1-5)
+            # toggle value if keypressed. False = Draw, True = Hold
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    choices[0] = not choices[0]
+                elif event.key == pygame.K_2:
+                    choices[1] = not choices[1]
+                elif event.key == pygame.K_3:
+                    choices[2] = not choices[2]
+                elif event.key == pygame.K_4:
+                    choices[3] = not choices[3]
+                elif event.key == pygame.K_5:
+                    choices[4] = not choices[4]
+                elif event.key == pygame.K_SPACE:
+                    returnValue = draw(choices, hand, deck)
+                    choices = returnValue[0]
+                    hand = returnValue[1]
+                    deck = returnValue[2]
+                    print (choices)
+                    print (hand)
+                    renderHand(hand)
+                    pygame.display.update()
+                    r = rank(hand)
+                    handRank = r[0]
+                    print("%-18s %-15s %s" % ("HAND", "CATEGORY", "TIE-BREAKER"))
+                    # for cards in hand:
+                    #     r = rank(cards)
+                    print("%-18r %-15s %r" % (hand, r[0], r[1]))
+                    pygame.draw.rect(screen, WHITE, [270, 40, 255, 90],2)
+                    score_text = small_font.render("Hand Rank = " + handRank, True, BLACK)
+                    score_text_rect = score_text.get_rect()
+                    score_text_rect.center = (WIDTH//2, 85)
+                    screen.blit(score_text, score_text_rect)
+                elif event.key == pygame.K_RETURN:
+                    screen.fill(GRAY)
+                    newGame = True
 
-                print (choice)
-                print (hand)
-                renderHand(hand)
-                pygame.display.update()
-                r = rank(hand)
-                handRank = r[0]
-                print("%-18s %-15s %s" % ("HAND", "CATEGORY", "TIE-BREAKER"))
-                # for cards in hand:
-                #     r = rank(cards)
-                print("%-18r %-15s %r" % (hand, r[0], r[1]))
-                pygame.draw.rect(screen, WHITE, [270, 40, 255, 90],2)
-                score_text = small_font.render("Hand Rank = " + handRank, True, BLACK)
-                score_text_rect = score_text.get_rect()
-                score_text_rect.center = (WIDTH//2, 85)
-                screen.blit(score_text, score_text_rect)
-            elif event.key == pygame.K_RETURN:
+                set_buttons(choices)
 
 
 
-    # Update the display after each game loop
-    pygame.display.update()
+
+
+        # Update the display after each game loop
+        pygame.display.update()
